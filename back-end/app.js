5aa43ec1750289f3c6cdc8c04b5985f0bd1ab06b
DayannaRojas123 <rojasdahianna123@gmail.com>
2023-03-22 17:21:17 -0500

    CRUD 2.0

--- back-end/app.js
+++ back-end/app.js
@@ -1,20 +1,27 @@
-const express=require('express')
-const cors=require('cors')
-const morgan=require('morgan')
-const app=express()
-const routes=require('./routes/routeUsers/route')
-const handleError=require('./handlers/handlerError')
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const app = express()
+const routes = require('./routes/routeUsers/route')
+const routesComment = require('./routes/routeComments/route')
+const handleError = require('./handlers/handlerError')
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 app.use(cors());
 app.use(morgan('tiny'));
 
-app.use('/',routes)
+app.use('/', routes)
+app.use('/', routesComment)
+
 app.use(handleError)
 
-const port=4000
+const port = 4000
 
-app.listen(port,console.log('el server escuchando por el puerto '+ port))
+app.listen(port, console.log('el server escuchando por el puerto ' + port))
 
-module.exports = { app };
+module.exports = {
+    app
+};
