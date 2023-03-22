721c57be61891db60e597cd3ea32aed5cd98a965
Carlos Navarro <kandres38@gmail.com>
2023-03-22 18:17:02 -0500

    controladores signIn

--- back-end/controllers/user/singnUpUserController.js
+++ back-end/controllers/user/singnUpUserController.js
@@ -3,6 +3,7 @@ const modeloUser= require('../../models').user;
 
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { json } = require('sequelize');
 exports.singUp = async(req,res,next)=>{
    // console.log(req.body)
 
@@ -10,17 +11,27 @@ exports.singUp = async(req,res,next)=>{
         let {name,last_name,nickname,email,password1,password2}=req.body;
 
         password1 = bcrypt.hashSync(password1,10);
-       // res.send(password1)
+        password2 = bcrypt.hashSync(password2,10);
+
+        let user=await modeloUser.findOne({ where: {email } });
         
-        await modeloUser.create({
-            nickname,name,last_name,email,password1
-        }).then((data)=>{
-           let token= jwt.sign({
-                data
-              }, 'secret', { expiresIn: '1h' });
-            
-            res.status(201).json({data,token})
-        }).catch((err) => next(err));
+            if(!user){
+                res.status(100).json({message:'Usuario ya existe'})
+            }else{
+
+                modeloUser.create({
+                    nickname,name,last_name,email,password1,password2
+                }).then((data)=>{
+                   let token= jwt.sign({
+                        data
+                      }, 'secret', { expiresIn: '1h' });
+                    
+                    res.status(201).json({data,token})
+                }).catch((err) => next(err));
+
+            }
+              
+        
         
     } catch (error) {
         res.send(error)
