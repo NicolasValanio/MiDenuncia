75088bbc4f858438f57dcbb21aede378a0e47502
Carlos Navarro <kandres38@gmail.com>
2023-03-23 12:29:14 -0500

    trabajo de auth0.. en formacion

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,18 @@
+const dotenv = require('dotenv');
 const express = require('express')
 const cors = require('cors')
 const sgMail=require('./services/sendgrid')
+const { auth } = require('express-openid-connect');
 const morgan = require('morgan')
 const app = express()
 const routes = require('./routes/routeUsers/route')
 const routesComment = require('./routes/routeComments/route')
 const routeRequest=require('./routes/routeRequest/route')
 const handleError = require('./handlers/handlerError')
+const users = require('./routes/routeUsers/routeUSerAuth0')
+dotenv.config()
 
+const config=require('./middleware/authUser')
 app.use(express.urlencoded({
     extended: true
 }));
@@ -18,7 +23,8 @@ app.use(cors());
 
 app.use(morgan('tiny'));
 
-app.use('/', routes)
+app.use('/',auth(config),users )
+app.use('/',auth(config),routes)
 app.use('/',routeRequest)
 app.use('/', routesComment)
 
--- back-end/middleware/authUser.js
+++ back-end/middleware/authUser.js
@@ -1,7 +1,9 @@
-
-authRequired: false,
+module.exports= config = {
+    authRequired: false,
     auth0Logout: true,
     secret: '6KTAmUOwWQu6W3wKCom4b17bwiiDGlz4DA5Ed3QJHsTOdMSw4TaXmGUT8VLq_lOM',
     baseURL: 'http://localhost:4000',
     clientID: '9WrLtkBBZw1ictNMG24ArmMD4LglFrkK',
     issuerBaseURL: 'https://dev-c7jccwgb5x7xqvsp.us.auth0.com'
+  };
+  
