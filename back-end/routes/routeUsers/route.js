fa97bf4b8557abdbd505f31c3daf826e6eb7234d
Carlos Torres <juanagelves@gmail.com>
2023-03-23 16:07:44 -0500

    parte2

--- back-end/routes/routeUsers/route.js
+++ back-end/routes/routeUsers/route.js
@@ -5,13 +5,14 @@ const authMiddleware=require('../../middleware/auth')
 const signUpUserController=require('../../controllers/user/singnUpUserController');
 const infoUserController=require('../../controllers/user/infoUserController');
 const signInUserController= require('../../controllers/user/signInUserController');
-
-
+const infoRequestUser = require('../../controllers/user/infoRequestUserController');
 
 
 
 router.post('/createUser',signUpUserController.singUp)
 router.get('/signIn',signInUserController.signIn)
 router.get('/info',authMiddleware,infoUserController.infoUser)
+router.get('/infoRequestUser',infoRequestUser.infoRequestUser)
+
 
 module.exports=router
