204537aba12a798601083a543241e2e08ce40267
Carlos Navarro <kandres38@gmail.com>
2023-03-23 18:50:59 -0500

    ajustes locales de directorios

--- back-end/middleware/authUser.js
+++ back-end/middleware/authUser.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
 module.exports= config = {
     authRequired: false,
     auth0Logout: true,
-    secret: '6KTAmUOwWQu6W3wKCom4b17bwiiDGlz4DA5Ed3QJHsTOdMSw4TaXmGUT8VLq_lOM',
-    baseURL: 'http://localhost:4000',
-    clientID: '9WrLtkBBZw1ictNMG24ArmMD4LglFrkK',
-    issuerBaseURL: 'https://dev-c7jccwgb5x7xqvsp.us.auth0.com'
+    secret: process.env.AUTH_SECRET,
+    baseURL: process.env.AUTH_BASE_URL,
+    clientID: process.env.AUTH_CLIENT_ID,
+    issuerBaseURL: process.env.AUTH_BASE_URL 
+    
   };
+  
+  console.log(config);
   
