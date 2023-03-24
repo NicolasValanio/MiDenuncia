6e83d17e49a16a2d13d087e15331e13f9632b173
Carlos Torres <juanagelves@gmail.com>
2023-03-24 17:35:36 -0500

    Arreglar errores google

--- back-end/app.js
+++ back-end/app.js
@@ -1,10 +1,12 @@
 const express=require('express')
-const cors=require('cors')
-const morgan=require('morgan')
+const cors = require('cors')
+const morgan = require('morgan')
+const passport = require('passport');
 const app=express()
 const routes=require('./routes/routeUsers/route')
 const routeRequest=require('./routes/routeRequest/route')
 const handleError=require('./handlers/handlerError')
+require('./middleware/auth2UserGoogle')
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
@@ -14,12 +16,54 @@ app.use(cors());
 
 app.use(morgan('tiny'));
 
-app.use('/',routes)
-app.use('/',routeRequest)
-app.use(handleError)
+// app.use('/',routes)
+// app.use('/',routeRequest)
+// app.use(handleError)
 
 
+// ------------------------------ Google OAuth2 ------------------------------ 
 
+// app.use('/',middlewareAuth2User,(req,res) => {
+//     res.send('hola')
+// });
+
+// app.get('/',(req,res) => {
+//     res.send('<a href="/auth/google">Entrar por google</a>');
+// })
+
+
+// app.get('/auth/google',
+//   passport.authenticate('google', { scope:
+//       [ 'email', 'profile' ] }
+// ));
+
+// app.get( '/auth/google/callback',
+//     passport.authenticate( 'google', {
+//         successRedirect: '/auth/google/success',
+//         failureRedirect: '/auth/google/failure'
+// }));
+
+// app.get('/auth/google',
+//     passport.authenticate('google', {scope: ['email', 'password']})
+// )
+
+app.get('/',(req,res) => {
+    res.send('<a href="/auth/google">Entrar por google</a>');
+})
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/proteced');
+  });
+
+  app.get('/proteced', (req,res) => {
+    res.send('Holaaaa');
+});
 
 
 
