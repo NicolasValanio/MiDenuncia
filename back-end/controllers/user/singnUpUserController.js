79e5dc5711e38cc3a81dcd1ea4f848c48b095641
Carlos Torres <juanagelves@gmail.com>
2023-03-27 16:20:57 -0500

    ajuste user

--- back-end/controllers/user/singnUpUserController.js
+++ back-end/controllers/user/singnUpUserController.js
@@ -6,25 +6,25 @@ const jwt = require('jsonwebtoken');
 const { json } = require('sequelize');
 exports.singUp = async(req,res,next)=>{
    // console.log(req.body)
-
     try {
         let {name,last_name,nickname,email,password}=req.body;
 
         password = bcrypt.hashSync(password,10);
-       
-
-        let user=await modeloUser.findOne({ where: {email } });
+        //let user=await modeloUser.findOne({ where: {email } });
         
             // if(!user){
             //     res.status(100).json({message:'Usuario ya existe'})
             // }else{
 
-                modeloUser.create({
-                    nickname,name,last_name,email,password,
+                const [user,created] = modeloUser.findOrCreate({
+                    
+                    where : nickname,name,last_name,email,password,
+                    default : nickname,name,last_name,email,password,
+
                 }).then((data)=>{
-                   let token= jwt.sign({
+                let token= jwt.sign({
                         data
-                      }, 'secret', { expiresIn: '1h' });
+                    }, 'secret', { expiresIn: '1h' });
                     
                     res.status(201).json({data,token})
                 }).catch((err) => next(err));
--- back-end/middleware/auth2UserGoogle.js
+++ back-end/middleware/auth2UserGoogle.js
@@ -5,7 +5,6 @@ const users=require('../models').user;
 const passport = require('passport');
 let GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-
 passport.serializeUser((user,done)=> {
   done(null,user)
 })
@@ -19,37 +18,27 @@ passport.use(new GoogleStrategy({
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "/google/callback"
 
-   
   },
   async (accessToken, refreshToken, profile, cb) => {
     try {
-     
-       
-          
-           const [user, created] = await users.findOrCreate({
-             where: { nickname: profile.id,name:profile.displayName,last_name:profile.name.familyName,email:profile.emails[0].value,avatar:profile.photos[0].value },
-             defaults: { nickname:profile.id,name:profile.displayName,last_name:profile.name.familyName,email:profile.emails[0].value,avatar:profile.photos[0].value }
-           });
-           
-           if (created) {
-           
-           ///res.redirect('/login');
-            cb(null, user);
-          } else {
-            // Si se encontró un usuario existente, se podría redirigirlo a la página de inicio
-           // res.redirect('/login');
-            cb(null, user);
-          }
-          
-       
-       
-    //  }
 
-    
+          const [user, created] = await users.findOrCreate({
+            where: { nickname: profile.id,name:profile.displayName,last_name:profile.name.familyName,email:profile.emails[0].value,avatar:profile.photos[0].value },
+            defaults: { nickname:profile.id,name:profile.displayName,last_name:profile.name.familyName,email:profile.emails[0].value,avatar:profile.photos[0].value }
+          });
+          
+          if (created) {
+          ///res.redirect('/login');
+          cb(null, user);
+          } else {
+          // Si se encontró un usuario existente, se podría redirigirlo a la página de inicio
+          // res.redirect('/login');
+          cb(null, user);
+          }
+    //  }
      //con esto retorno el perfil par que passport lo utilize
-    
     } catch (err) {
-       cb(err);
+      cb(err);
     }
   }
 ));
