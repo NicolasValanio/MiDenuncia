dc9a7d016de73ca86049ecf3acbc0d1fcb563fae
PAN77 <germanm1853@gmail.com>
2023-03-27 16:07:02 -0500

    tarjetas borrado

--- Front-end/src/componentes/tarjetasPublicacion/fech.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const userget= ()=>{
-const url='https://midenuncia-database-production.up.railway.app/infoRequestUser'
-let a = axios.get(url).then(res=> res ).catch(err=> console.error(err));
-return a
-}
-
