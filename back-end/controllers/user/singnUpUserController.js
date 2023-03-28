25b8172b16e9f2d48cd9170ae935783b99318b0b
Carlos Torres <juanagelves@gmail.com>
2023-03-27 21:22:46 -0500

    Ajustes user

--- back-end/controllers/user/singnUpUserController.js
+++ back-end/controllers/user/singnUpUserController.js
@@ -2,43 +2,34 @@ const modeloUser= require('../../models').user;
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { json } = require('sequelize');
-exports.singUp = async(req,res,next)=>{
+exports.signUp = async(req,res,next)=>{
    // console.log(req.body)
 
-exports.signUp = async (req, res, next) => {
-  try {
-    let {name, last_name, nickname, email, password} = req.body;
+    try {
+        let {name,last_name,nickname,email,password}=req.body;
 
-    // buscar el correo electrónico en la tabla de usuarios
-    const user = await modeloUser.findOne({where: {email: email}});
-
-    // si el correo electrónico existe, enviar una respuesta de error
-    if (user) {
-      return res.status(400).json({message: 'El correo electrónico ya está registrado'});
-    }
+        password = bcrypt.hashSync(password,10);
 
         let user=await modeloUser.findOne({ where: {email } });
         
-            // if(!user){
-            //     res.status(100).json({message:'Usuario ya existe'})
-            // }else{
+            if(user){
+                res.status(400).json({message:'Usuario ya existe'})
+            }else{
 
-                modeloUser.create({
+              await  modeloUser.create({
                     nickname,name,last_name,email,password,
                 }).then((data)=>{
-                   let token= jwt.sign({
+                  let token= jwt.sign({
                         data
                       }, 'secret', { expiresIn: '1h' });
                     
                     res.status(201).json({data,token})
-                }).catch((err) => next(err));
+                }).catch((err) => {
+                    res.status(400).json({message:err.message})
+                });
 
-           // }
-              
-        
-        
+            }
     } catch (error) {
         res.send(error)
     }
 }
-
