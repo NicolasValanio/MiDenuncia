dc0288d062a639e57c69c9c7d3e00ce3b89c9734
Nicolas Valenzuela Ascanio <serviciosvalenzuela1@gmail.com>
2023-03-29 00:21:10 -0500

    redirección de rutas

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,53 +1,14 @@
 import './App.css'
 import Rutas from '../src/componentes/rutas'
 
-<<<<<<< HEAD
-
-import Login from './componentes/login/login'
-import UsuarioNoLog from './componentes/usuarioNoLog/usuarioNoLog'
-import HabeasData from './componentes/habeasData/habeasData'
-import PeticionesUsuarios from './componentes/peticionesUsuarios/peticionesUsuarios'
-import RegistroDenuncia from './componentes/registroDenuncia/registroDenuncia'
-import RegistroUsuario from './componentes/registroUsuario/registroUsuario'
-import UsuarioLog from './componentes/usuarioLog/usuarioLog'
-import VistaDenunciaX from './componentes/vistaDenunciaX/vistaDenunciaX'
-import VistaSuperAdmin from './componentes/vistaSuperAdmin/vistaSuperAdmin'
-import VistaUsuario from './componentes/vistaUsuario/vistaUsuario'
-import NavegacionNoLog  from './componentes/navegacionNoLog/navegacion';
-import NavegacionUserLog from './componentes/navegacionUserLog/NavegacionUserLog'
-=======
->>>>>>> origin
 // NADIE TOCA ESTO!!!!
  function App() {
 // NADIE TOCA ESTO!!!!
   return (
     <div className="App">
-<<<<<<< HEAD
-
-      <Routes>
-        
-        <Route exact path="/" element={<UsuarioNoLog />} /> 
-        <Route path="/login" element={<Login />} />
-        <Route path="/HabeasData" element={<HabeasData />} />
-        <Route path="/PeticionesUsuarios" element={<PeticionesUsuarios />} />
-        <Route path="/RegistroDenuncia" element={<RegistroDenuncia />} />
-        <Route path="/RegistroUsuario" element={<RegistroUsuario />} />
-        <Route path="/UsuarioLog" element={<UsuarioLog />} />
-        <Route path='/UsuarioNoLog' element={<UsuarioNoLog />} />
-        <Route path="/VistaDenunciaX" element={<VistaDenunciaX />} />
-        <Route path="/VistaSuperAdmin" element={<VistaSuperAdmin />} />
-        <Route path="/VistaUsuario" element={<VistaUsuario />} />
-        <Route path="/NavegacionNoLog" element={<NavegacionNoLog />} />
-        <Route path="/NavegacionUserLog" element={<NavegacionUserLog />} />
-
-        <Route path="*" element={<h1>PAGINA NO EXISTE</h1>} />
-      </Routes>
-
-=======
 {/* // NADIE TOCA ESTO!!!! */}
       <Rutas />
 {/*  NADIE TOCA ESTO!!!! */}
->>>>>>> origin
     </div>
   ) 
 }
