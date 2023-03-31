cf508789a1f7404218a1ef16e3856280919fe700
neferyesid <neferysg@gmail.com>
2023-03-31 16:29:35 -0500

    borrando datos

--- Front-end/src/componentes/usuarioLog/usuarioLog.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function UsuarioLog(params) {
-    return (
-        <h1>UsuarioLog</h1>
-    )
-}
-
-export default UsuarioLog;
--- back-end/handlers/handlerError.js
+++
@@ -1,7 +0,0 @@
-
-module.exports=(err, req, res, next)=> {
-
-  
-  // send an error response to the client
-  res.status(500).send('usuario existente');
-  }
