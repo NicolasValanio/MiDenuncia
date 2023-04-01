15125c19924d3eac3128e2b8afc8bd370a59f2fb
Nicolas Valenzuela Ascanio <serviciosvalenzuela1@gmail.com>
2023-04-01 13:17:01 -0500

    Distribucion NoLog

---
+++ Front-end/src/componentes/tarjetasPublicacion/tarjetasPublicacion.jsx
@@ -0,0 +1,13 @@
+import React  from "react";
+import Style from "./tarjetasPublicacion.module.css"
+
+function  Tarjetas() {
+    return(
+        <div className={Style.tarjetas}>
+
+        </div>
+        )
+
+}
+
+export default Tarjetas;
