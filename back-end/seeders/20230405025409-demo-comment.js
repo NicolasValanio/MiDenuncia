d5de09b08330e0396e7483d464dce658d270c9e4
Ilva Perez <ilvaperezmiranda@gmail.com>
2023-04-05 00:22:11 -0500

    falta ordenar

--- back-end/seeders/20230405025409-demo-comment.js
+++ back-end/seeders/20230405025409-demo-comment.js
@@ -6,7 +6,7 @@ module.exports={
             {
                 date: new Date(),
                 description:'Excelente servicio',
-                state:'no se que decir',
+                status:'no se que decir',
                 request_id:1,
                 user_id:1,
                 createdAt: new Date(),
@@ -15,7 +15,7 @@ module.exports={
             {
                 date: new Date(),
                 description:'Excelente servicio',
-                state:'no se que decir',
+                status:'no se que decir',
                 request_id:2,
                 user_id:2,
                 createdAt: new Date(),
@@ -24,7 +24,7 @@ module.exports={
             {
                 date: new Date(),
                 description:'Excelente servicio',
-                state:'no se que decir',
+                status:'no se que decir',
                 request_id:3,
                 user_id:3,
                 createdAt: new Date(),
