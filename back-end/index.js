45038d2f48e6d26a520836a03237f5dbdefa0234
Carlos Torres <juanagelves@gmail.com>
2023-04-06 15:16:03 -0500

    pull carlos

--- back-end/index.js
+++ back-end/index.js
@@ -1,20 +1,17 @@
 
-<<<<<<< HEAD
-=======
-const app=require('./app')
-const bcrypt=require('bcrypt')
->>>>>>> c2c9fa2bd1323155a882f5a6e6bdd13b3678e803
+const app = require('./app')
+const bcrypt = require('bcrypt')
 
-const User=require('./models').user
-const SessionModel=require('./modeloMongo/sessionMongodb')
+const User = require('./models').user
+const SessionModel = require('./modeloMongo/sessionMongodb')
 
 ////aqui hacemos las importaciones y que todo quede dentro de ella
 const express = require('express');
 
-const mongoose= require('./services/mongoDB')
+const mongoose = require('./services/mongoDB')
 
 const session = require('express-session');
-const MongoDBStore  = require('connect-mongodb-session')(session);
+const MongoDBStore = require('connect-mongodb-session')(session);
 //const MongoStore = require('connect-mongo')(session);
 const Session = require('./services/sessionMongodb')
 
@@ -29,7 +26,7 @@ const dotenv = require('dotenv');
 //const { auth } = require('express-openid-connect');
 
 dotenv.config()
-const mongoose = require('mongoose');
+const mongoose = require('mongoose'); // --> Me genera error
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 
@@ -44,7 +41,7 @@ const morgan = require('morgan')
 ///aqui se LLAMAN A A LAS RUTAS las rutas generales y que todo quede dentro de ella
 const routes = require('./routes/routeUsers/route')
 const routesComment = require('./routes/routeComments/route')
-const routeRequest=require('./routes/routeRequest/route')
+const routeRequest = require('./routes/routeRequest/route')
 const routeEmail = require('./routes/routeEmail/nodemail')
 const routeAuthGoogle = require('./routes/routeGoogle/route')
 const handleError = require('./handlers/handlerError')
@@ -52,10 +49,10 @@ const handleError = require('./handlers/handlerError')
 
 //FIN
 ////////////////////////////////////////////////////////////////
- mongoose.connect(process.env.URI_MONGO, {
+mongoose.connect(process.env.URI_MONGO, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
- // useCreateIndex: true
+  // useCreateIndex: true
 });
 
 
@@ -63,17 +60,19 @@ app.use(session({
   secret: 'mysecret', // secreto para firmar las cookies de sesión
   resave: false,
   saveUninitialized: true,
-  store: MongoStore.create({ mongoUrl: process.env.URI_MONGO,
-  crypto: {
-    secret: 'secret'
-  },
-  collection: 'sessions',
-  // expires: 60 * 60 * 24 * 7, // 7 days
-  expires: 120, // 2 minutes
-  model: SessionModel,
-  
-  // ttl: 24 * 60 * 60, // 1 día de vida útil })
-})}));
+  store: MongoStore.create({
+    mongoUrl: process.env.URI_MONGO,
+    crypto: {
+      secret: 'secret'
+    },
+    collection: 'sessions',
+    // expires: 60 * 60 * 24 * 7, // 7 days
+    expires: 120, // 2 minutes
+    model: SessionModel,
+
+    // ttl: 24 * 60 * 60, // 1 día de vida útil })
+  })
+}));
 
 
 //////////////////////////////////
@@ -81,7 +80,7 @@ app.use(session({
 
 /// codigo especial para procesar solicitudes HTTP y expres json lo convierta en json
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
@@ -119,24 +118,24 @@ app.use(session({
 
   resave: true,
   saveUninitialized: true,
-  store: new MongoDBStore ({
+  store: new MongoDBStore({
     uri: 'mongodb+srv://midenuncia:MIDENUNCIA2023@api-session.gu6bn9e.mongodb.net/api-sessions',
     collection: 'mysessions',
-   
+
   }),
- 
- 
-// Guardar el documento en la colección
- 
+
+
+  // Guardar el documento en la colección
+
 }));
 
 ///aqui se configura la ENTRADA  A LAS RUTAS trabajando  solo rutas
 app.use(cookieParser())
-app.use('/',routes)
-app.use('/',routeRequest)
+app.use('/', routes)
+app.use('/', routeRequest)
 app.use('/', routesComment)
-app.use('/',routeEmail)
-app.use('/',routeAuthGoogle)
+app.use('/', routeEmail)
+app.use('/', routeAuthGoogle)
 
 
 
@@ -156,7 +155,7 @@ app.use('/',routeAuthGoogle)
 //     sessionId: 'some_session_id',
 //     sessionData: { some_key: 'some_value' }
 //   });
-  
+
 //   session.save()
 //     .then(() => {
 //       console.log('La sesión se ha guardado correctamente');
@@ -187,25 +186,25 @@ app.get('/verificacionToken', async (req, res) => {
   const { v4: uuidv4 } = require('uuid');
 
   //const uniqueId = uuidv4();
-  const {token,email} = req.query;
+  const { token, email } = req.query;
 
-   await User.findOne({where: {resetPasswordToken:token}})
-  .then(user => {
-    const user1=new SessionModel
-    user1.sessionID=req.sessionID
-    user1.session=user.email
-    identificadorUUI=uuidv4()
-    user1.save()
-    req.session.email=user.email
-   
-    
-    const url = `http://localhost:5173/contrasenaNueva?token=${user.resetPasswordToken}&email=${user.email}}`;
-  res.redirect(url)
-  }).catch(err => {
-    res.send('token no funciona')
-  });
- 
-  
+  await User.findOne({ where: { resetPasswordToken: token } })
+    .then(user => {
+      const user1 = new SessionModel
+      user1.sessionID = req.sessionID
+      user1.session = user.email
+      identificadorUUI = uuidv4()
+      user1.save()
+      req.session.email = user.email
+
+
+      const url = `http://localhost:5173/contrasenaNueva?token=${user.resetPasswordToken}&email=${user.email}}`;
+      res.redirect(url)
+    }).catch(err => {
+      res.send('token no funciona')
+    });
+
+
 
 })
 
@@ -213,42 +212,42 @@ app.get('/verificacionToken', async (req, res) => {
 //INICIO
 app.put('/newPassword', async (req, res) => {
 
-  let {password, password2} = req.body;
+  let { password, password2 } = req.body;
 
 
 
 
-//console.log(email);
-const seesionid = Object.keys(req.sessionStore.sessions)[0]
-//console.log(seesionid)
+  //console.log(email);
+  const seesionid = Object.keys(req.sessionStore.sessions)[0]
+  //console.log(seesionid)
 
-const sessioUser= await SessionModel.findOne({ sessionID: seesionid })
-  .then(user => {
-   
-    
-   
-      User.findOne({ where: {email:user.session}})
-        .then(user=>{
-          if(user){
-           
-            password2 = bcrypt.hashSync(password2,10);
-             User.update({password: password2},
-              {where: {email: user.email}})
-              .then(user => res.status(200).json({ message: 'cambio de contraseña exitoso!'}))
+  const sessioUser = await SessionModel.findOne({ sessionID: seesionid })
+    .then(user => {
+
+
+
+      User.findOne({ where: { email: user.session } })
+        .then(user => {
+          if (user) {
+
+            password2 = bcrypt.hashSync(password2, 10);
+            User.update({ password: password2 },
+              { where: { email: user.email } })
+              .then(user => res.status(200).json({ message: 'cambio de contraseña exitoso!' }))
               .catch(err => res.json({ message: err.message }))
-          }else{
+          } else {
             res.status(400).json({ message: "no se pudo" })
           }
 
-        }).catch(err => res.json({ message: err.message}))
-      
+        }).catch(err => res.json({ message: err.message }))
 
 
-  })
-  .catch(error => console.error('Error al buscar usuario', error));
+
+    })
+    .catch(error => console.error('Error al buscar usuario', error));
 
 
-  
+
 })
 
 app.get("/logout", (req, res) => {
@@ -256,9 +255,9 @@ app.get("/logout", (req, res) => {
     if (err) {
       console.log(err);
     } else {
-     // console.log("Sesión finalizada correctamente");
+      // console.log("Sesión finalizada correctamente");
       // Eliminar el token de autenticación de la base de datos
-      
+
       res.redirect("http://localhost:5173/login");
     }
   });
