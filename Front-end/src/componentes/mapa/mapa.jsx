3ba8a98f8e9f9f0a585d0092a7bbebd8cb122710
Nicolas Valenzuela Ascanio <serviciosvalenzuela1@gmail.com>
2023-04-11 14:44:51 -0500

    CAMBIOS Mapas

--- Front-end/src/componentes/mapa/mapa.jsx
+++ Front-end/src/componentes/mapa/mapa.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 import Style from "./mapa.module.css";
+import {GoogleMap} from "@react-google-maps/api"
 
-
-function Mapa(){
+var latitud= 37.7749
+var lonitud= -122.4194
+const Mapa=()=>{
+    
+    const mapStyles = {
+        height: '400px',
+        width: '50%'
+    };
+    
+    const defaultCenter = {
+        lat: latitud,
+        lng: lonitud
+    };
+    
     return(
-    <h1>Soy un mapa</h1>
-)
-}
+        <div>
+            <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBA3h4EK-Yhre3R40tViWu9gQFQ-O98Jg4&callback=initMap"></script>
+            <GoogleMap 
+            mapContainerStyle={mapStyles}
+            center={defaultCenter}
+            zoom={6}
+            />
+        </div>
+    );
+    
+};
+console.log(<GoogleMap/>)
+
 
 export default Mapa;
