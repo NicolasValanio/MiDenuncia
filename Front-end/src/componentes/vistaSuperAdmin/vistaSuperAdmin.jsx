95ca1db35ec0724e11fc4f9af8c3f52249413bf5
Neferyesid <neferysg@gmail.com>
2023-04-14 17:07:13 -0500

    nuevo

--- Front-end/src/componentes/vistaSuperAdmin/vistaSuperAdmin.jsx
+++ Front-end/src/componentes/vistaSuperAdmin/vistaSuperAdmin.jsx
@@ -11,58 +11,61 @@ function VistaSuperAdmin() {
         .then((data) => setData(data));
     }, []);
 
-
-
+   
     return(
         <div className={style.contenedor1}>
-            <div className={style.name}>@SuperAdmin</div>
-            
-            <div>
-            <BiSearchAlt className={style.iconBuscar}/><input className={style.input} type="text" placeholder="      Filtrar" />
-           <div className={style.filtrar}>
-           <select name="" id="" >
-                <option value="">Nombre</option>
-                <option value="">Usuario</option>
-                <option value="">Barrio</option>
-                <option value="">Rol</option>
-                <option value="">Estado</option>
-            </select>
-            </div>
-           
-           </div>
-            
-            <div className={style.barra}>
-                <ul className={style.opciones}>
-                    <li>Nombre</li>
-                    <li>Usuario</li>
-                    <li>DIreccion</li>
-                    <li>Correo</li>
-                    <li>Rol</li>
-                    <li>Estado</li>
-                </ul>   
-            </div>
-            <div className={style.barra}>
-                <div className={style.opciones}>
-                    <div>
-                    {data?.map((user) => (
-                        <div key={user.id}>{user.name}</div>
-                    ))}
+           <div className={style.superAdmin}>@SuperAdmin</div>
+           <div className={style.contenedorFiltrar}>
+                    <div className={style.conteInput}>
+                        <span><BiSearchAlt className={style.iconBuscar}/></span>
+                        <input className={style.input} type="text" placeholder="Filtrar"/>
                     </div>
-                    
-                    <li>nea</li>
-                    <li>cumbre</li>
-                    <li>Correo</li>
-                    <select name="" id="" className={style.rol}>
-                        <option value="">Visitante</option>
-                        <option value="">Administador</option>
-                    </select>
-                    <select name="" id="" className={style.rol}>
-                        <option value="">Activo</option>
-                        <option value="">Inactivo</option>
-                    </select>
-                </div>
+                    <div className={style.filtrar}>
+                        <select name="" id="" >
+                            <option value="">Nombre</option>
+                            <option value="">Usuario</option>
+                            <option value="">Barrio</option>
+                            <option value="">Rol</option>
+                            <option value="">Estado</option>
+                        </select>
+                    </div>
+            </div >
+                
+                <div className={style.barras}>
+                    <div className={style.barra}>
+                        <ul className={style.opciones}>
+                            <li>Nombre</li>
+                            <li>Usuario</li>
+                            <li>DIreccion</li>
+                            <li>Correo</li>
+                            <li>Rol</li>
+                            <li>Estado</li>
+                        </ul>   
+                    </div>
+                    <div className={style.barra}>
+                        <div className={style.opciones}>
+                            <div>
+                            {data?.map((user) => (
+                                <div  key={user.name}>{user.name}</div>
+                            ))}
+                        
+                            </div>
+                            
+                            {/* <li>nea</li>
+                            <li>cumbre</li>
+                            <li>Correo</li>
+                            <select name="" id="" className={style.rol}>
+                                <option value="">Visitante</option>
+                                <option value="">Administador</option>
+                            </select>
+                            <select name="" id="" className={style.rol}>
+                                <option value="">Activo</option>
+                                <option value="">Inactivo</option>
+                            </select> */}
+                        </div>
 
-            </div>
+                    </div>
+                </div>
         </div>
     )
 }
