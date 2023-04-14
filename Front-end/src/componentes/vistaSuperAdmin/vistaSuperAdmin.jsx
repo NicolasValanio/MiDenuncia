c8dadf59d6daa7f6b62dd57ebf2f1ecf67e48998
Neferyesid <neferysg@gmail.com>
2023-04-14 15:53:10 -0500

    nuevo

--- Front-end/src/componentes/vistaSuperAdmin/vistaSuperAdmin.jsx
+++ Front-end/src/componentes/vistaSuperAdmin/vistaSuperAdmin.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import style from "./vistaSuperAdmin.module.css"
 import { BiSearchAlt } from "react-icons/bi";
 
 function VistaSuperAdmin() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      fetch("https://midenuncia-database-production.up.railway.app/info")
+        .then((response) => response.json())
+        .then((data) => setData(data));
+    }, []);
+
+
+
     return(
         <div className={style.contenedor1}>
-            <h1 className={style.h1}>@SuperAdmin</h1>
+            <div className={style.name}>@SuperAdmin</div>
             
-           <BiSearchAlt className={style.iconBuscar}/><input className={style.input} type="text" placeholder="      Filtrar" />
+            <div>
+            <BiSearchAlt className={style.iconBuscar}/><input className={style.input} type="text" placeholder="      Filtrar" />
            <div className={style.filtrar}>
            <select name="" id="" >
                 <option value="">Nombre</option>
@@ -16,6 +27,8 @@ function VistaSuperAdmin() {
                 <option value="">Rol</option>
                 <option value="">Estado</option>
             </select>
+            </div>
+           
            </div>
             
             <div className={style.barra}>
@@ -26,11 +39,16 @@ function VistaSuperAdmin() {
                     <li>Correo</li>
                     <li>Rol</li>
                     <li>Estado</li>
-                </ul>
+                </ul>   
             </div>
             <div className={style.barra}>
-            <ul className={style.opciones}>
-                    <li>johan</li>
+                <div className={style.opciones}>
+                    <div>
+                    {data?.map((user) => (
+                        <div key={user.id}>{user.name}</div>
+                    ))}
+                    </div>
+                    
                     <li>nea</li>
                     <li>cumbre</li>
                     <li>Correo</li>
@@ -42,7 +60,7 @@ function VistaSuperAdmin() {
                         <option value="">Activo</option>
                         <option value="">Inactivo</option>
                     </select>
-                </ul>
+                </div>
 
             </div>
         </div>
