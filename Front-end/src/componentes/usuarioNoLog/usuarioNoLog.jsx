3d90230f9c5ae38e9c7e8bd3512ffbbec9beb486
johanSarmiento <johansarmiento47@gmail.com>
2023-04-17 09:02:08 -0600

    tarjetas en usuariNoLog

--- Front-end/src/componentes/usuarioNoLog/usuarioNoLog.jsx
+++ Front-end/src/componentes/usuarioNoLog/usuarioNoLog.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Style from "./usuarioNoLog.module.css"
 import NavegacionNoLog from "../navegacionNoLog/navegacion";
 import FiltrarPor from "../filtrarPor/filtarPor";
@@ -6,13 +6,30 @@ import Footer from "../footer/footer";
 import Tarjetas from "../tarjetasPublicacion/tarjetasPublicacion";
 
 function UsuarioNoLog() {
+
+    const [publicaciones, setPublicaciones] = useState()
+
+    useEffect((()=>{
+        fetch(`https://midenuncia-database-production.up.railway.app/infoRequestUser?limit=10&offset=0`)
+        .then(res => res.json())
+        .then(res => setPublicaciones(res.news))
+    }),[])
+
+    function tarjetasNoLog() {
+        let nuevasTarjetas = publicaciones.map(()=>{
+            return <Tarjetas />
+        })
+        return nuevasTarjetas
+    }
+
     return (
             <div>
                 <NavegacionNoLog/>
                 <div className={Style.main}>
                 <FiltrarPor/>    
                     <div className={Style.contTarjetas}>
-                    {/* <Tarjetas /> */}
+
+                    { publicaciones === undefined ? null : tarjetasNoLog()}
 
                     </div>
                 </div>
